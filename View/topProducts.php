429078319fa7403d881ec270300e99704743beb9
Georgi Karastoyanov <georgy.karastoyanov@gmail.com>
2019-03-03 18:36:06 +0200

    added bootstrap cards

--- View/topProducts.php
+++ View/topProducts.php
@@ -1,19 +1,31 @@
 <?php
 $topProducts = \model\ProductDao::getTopProducts();
 ?>
-<h1>Best Sellers</h1>
-<table>
-    <tr>
-        <th>Image</th>
-        <th>ProductName</th>
-        <th>Details</th>
-    </tr>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1>Best Sellers</h1>
+<div class="card-deck" style="margin-left: 50px">
     <?php foreach ( $topProducts as $favorite) { ?>
-            <tr>
-                <td><?=$favorite['img_uri'] ?></td>
-                <td><?=$favorite['productName'] ?></td>
-                <td><a href=""></a>Details</td>
-            </tr>
-            <?php } ?>
-</table>
+    <div class="container, mh-20">
+    <h6><?=$favorite['productName'] ?></h6>
+    <div class="card" style="width:180px">
+        <img class="card-img-top" src="<?=$favorite['img_uri'] ?>" alt="Card image" style="width:80%">
+        <div class="card-body">
+            <a href="?target=product&action=getProduct&productId=<?php echo $favorite['id'];?>" class="btn btn-primary , stretched-link">View product</a>
+            <a> Price : <?=$favorite['price'] ?> $</a>
+        </div>
+    </div>
+    </div>
+<?php } ?>
+</div>
