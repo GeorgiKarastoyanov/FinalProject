877e9a8d4e3246516718fe83ad01e0e1470bac2b
Nayden1987 <nayden.dimitrov1987@gmail.com>
2019-03-03 18:45:58 +0200

    update footer

--- View/footer.php
+++ View/footer.php
@@ -1,83 +1,60 @@
-<?php
-?>
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Footer</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 </head>
+
 <body>
-
-<footer>
-    <table id="table-orders">
-    <tr>
-        <th>Orders</th>
-    </tr>
-    <tr>
-        <td><a href="View/account_orders.php">My Orders</a></td>
-    </tr>
-    <tr>
-        <td><a href="View/account_profile.php">My Account</a></td>
-    </tr>
-    <tr>
-        <td><a href="">F A Q</a></td>
-    </tr>
-</table>
-<table id="table-contacts">
-    <tr>
-        <th>Contacts</th>
-    </tr>
-    <tr>
-        <td><a href="https://www.emag.bg/info/za-vrazka-s-nas?ref=footer_3_1" target="_blank">Information</a></td>
-    </tr>
-    <tr>
-        <td><a href="">Phone: +359 888 88 88 88 </a></td>
-    </tr>
-</table>
-<table id="table-our_partners">
-    <tr>
-        <th>Our Partners</th>
-    </tr>
-    <tr>
-        <td><a href="https://www.samsung.com/bg/" target="_blank">Samsung</a></td>
-    <tr>
-        <td><a href="https://www.zara.com/bg/en/" target="_blank">Zara</a></td>
-    </tr>
-
-    <tr>
-        <td><a href="https://www.bosch.bg/" target="_blank">Bosh</a></td>
-    </tr>
-</table>
-
-
-<!---->
-<!--<div id="footer_orders" style="float: left; margin-right: 30%">-->
-<!--    <label for=""style="font-size: 1.3em; text-align: center" >Orders</label>-->
-<!--    <ul>-->
-<!--        <li><a href="View/account_orders.php">My Orders</a></li>-->
-<!--        <li><a href="View/account_profile.php">My Account</a></li>-->
-<!--        <li><a href="">F A Q</a></li>-->
-<!--    </ul>-->
-<!--</div>-->
-<!--<div id="footer_contacts" style="float: left; margin-right: 30%">-->
-<!--    <label for="">Contacts</label>-->
-<!--    <ul>-->
-<!--        <li><a href="https://www.emag.bg/info/za-vrazka-s-nas?ref=footer_3_1">Information</a></li>-->
-<!--        <li><a href="">Phone: +359 888 88 88 88 </a></li>-->
-<!--    </ul>-->
-<!--</div>-->
-<!--<div id="footer_our_partners">-->
-<!--    <label for="">Our Partners</label>-->
-<!--    <ul>-->
-<!--        <li><a href="https://www.samsung.com/bg/">Samsung</a></li>-->
-<!--        <li><a href="https://www.zara.com/bg/en/">Zara</a></li>-->
-<!--        <li><a href="https://www.bosch.bg/">Bosh</a></li>-->
-<!--    </ul>-->
-<!--</div>-->
+<div class="content">
+</div>
+<footer id="myFooter">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 myCols">
+                <h5>Get started</h5>
+                <ul>
+                    <li><a href="?target=user&action=register_email_view">Sign up</a></li>
+                    <li><a href="?target=user&action=login_email_view">Log in</a></li>
+                    <li><a href="?target=home&action=account">My Account</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3 myCols">
+                <h5>About us</h5>
+                <ul>
+                    <li><a href="https://www.emag.bg/info/za-vrazka-s-nas?ref=footer_3_1" target="_blank">Company
+                            Information</a></li>
+                    <li><a href="https://www.emag.bg/info/form/reklamatsii-i-izvestia" target="_blank">Contact us</a>
+                    </li>
+                    <li><a href="#">Reviews</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3 myCols">
+                <h5>Support</h5>
+                <ul>
+                    <li><a href="https://www.emag.bg/info/topic/chesto-zadavani-vaprosi" target="_blank">FAQ</a></li>
+                    <li><a href="https://www.emag.bg/info/" target="_blank">Help desk</a></li>
+                    <li><a href="https://www.emag.bg/info/topic/obshta-informatsia">News</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3 myCols">
+                <h5>Our Partners</h5>
+                <ul>
+                    <li><a href="https://www.samsung.com/bg/" target="_blank">Samsung</a></li>
+                    <li><a href="https://www.zara.com/bg/en/" target="_blank">Zara Fashion</a></li>
+                    <li><a href="https://www.bosch.bg/" target="_blank">Bosh</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
-</html>
 
+</html>
