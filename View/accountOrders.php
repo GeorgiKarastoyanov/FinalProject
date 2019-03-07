df117363e11d7a35eb186c1e195b386a663f3a37
Nayden1987 <nayden.dimitrov1987@gmail.com>
2019-03-07 21:40:15 +0200

    stylization my profile,my orders and details

--- View/accountOrders.php
+++ View/accountOrders.php
@@ -1,15 +1,35 @@
-<table>
-    <tr>
-        <th>Order Id</th>
-        <th>Date</th>
-        <th>Details</th>
-    </tr>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Orders</title>
+    <link rel="stylesheet" href="View/css/my-orders.css">
+</head>
+<body>
+<div id="my-orders-account">
+    <img src="View/images/logo-login.png" id="my-acc-logo-img" alt="eMAG">
+    <h3>My Orders</h3>
+    <table id="table-my-orders">
+        <tr>
+            <th>Order Id</th>
+            <th>Date</th>
+            <th>Details</th>
+        </tr>
 
-<?php foreach ($params['orders'] as $order) { ?>
-    <tr>
-        <td><?=$order['id'] ?></td>
-        <td><?=$order['date'] ?></td>
-        <td><button><a href='?target=product&action=orderDetails&order=<?=$order['id']?>'>Order Details</a></button></td>
-    </tr>
-   <?php } ?>
-</table>
+        <?php foreach ($params['orders'] as $order) { ?>
+            <tr>
+                <td><?= $order['id'] ?></td>
+                <td><?= $order['date'] ?></td>
+                <td>
+                    <button><a href='?target=product&action=orderDetails&order=<?= $order['id'] ?>'>Order Details</a>
+                    </button>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+</div>
+</body>
+</html>
--- View/accountProfile.php
+++ View/accountProfile.php
@@ -1,15 +1,53 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Profile</title>
+    <link rel="stylesheet" href="View/css/my-profile.css">
+</head>
+<body>
+<div id="my-profile-account">
+    <img src="View/images/logo-login.png" id="account-logo-img" alt="eMAG">
+    <form action="?target=user&action=editProfile" method="post" class="form">
+        <h3 id="my-prof-text">My Profile</h3>
+        <table id="table-my-profile">
+            <tr>
+                <td><label for="">Password:</label></td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td><label for="">Confirm Password:</label></td>
+                <td><input type="password" name="confirm-password"></td>
+            </tr>
+            <tr>
+                <td><label for="">Email:</label></td>
+                <td><input type="email" name="email"
+                            value="<?= isset($_SESSION['user']['email']) ? $_SESSION['user']['email'] : "" ?>"></td>
+            </tr>
+            <tr>
+                <td><label for="">First Name:</label></td>
+                <td><input type="text" name="first-name"
+                            value="<?= isset($_SESSION['user']['firstName']) ? $_SESSION['user']['firstName'] : "" ?>"></td>
+            </tr>
+            <tr>
+                <td><label for="">Last Name:</label></td>
+                <td><input type="text" name="last-name"
+                               value="<?= isset($_SESSION['user']['lastName']) ? $_SESSION['user']['lastName'] : "" ?>"></td>
+            </tr>
+            <tr>
+                <td><label for="">Address:</label></td>
+                <td><input type="text" name="address"
+                           value="<?= isset($_SESSION['user']['address']) ? $_SESSION['user']['address'] : "" ?>"></td>
+            </tr>
+        </table>
+        <input type="submit" id="submit-my-profile" name="edit-profile" value="Save Edit">
+        <?= $_SESSION['user']['id'] != 1 ? "<a href=?target=user&action=delete'>Delete Account</a>" : "" ?>
 
-?>
+    </form>
+</div>
+</body>
+</html>
 
-<form action="?target=user&action=editProfile" method="post" class="form">
-    <h1>My Profile</h1>
-    Password:<input type="password" name="password" ><br>
-    Confirm-Pass:<input type="password" name="confirm-password"><br>
-    Email:<input type="email" name="email" value="<?=isset($_SESSION['user']['email']) ? $_SESSION['user']['email'] : ""?>"><br>
-    First Name:<input type="text" name="first-name" value="<?=isset($_SESSION['user']['firstName']) ? $_SESSION['user']['firstName'] : ""?>"><br>
-    Last Name:<input type="text" name="last-name" value="<?=isset($_SESSION['user']['lastName']) ? $_SESSION['user']['lastName'] : ""?>"><br>
-    Address:<input type="text" name="address" value="<?=isset($_SESSION['user']['address']) ? $_SESSION['user']['address'] : ""?>"><br>
-    <input type="submit" name="edit-profile" value="Save Edit">
-    <?= $_SESSION['user']['id'] != 1 ? "<a href=?target=user&action=delete'>Delete Account</a>" : "" ?>
-</form>
